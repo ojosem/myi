9b7ab105d842b5a2f7cfc229010dedcd03625547
Oliver Josem <omjosem@gmail.com>
2019-02-19 22:01:05 +1100

    Added name to Portfolio.Stock model

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,10 @@ from django.db import models
 
 class Stock(models.Model):
     code = models.CharField(max_length=10)
+    name = models.CharField(max_length=255)
     units = models.IntegerField()
     purchase_price = models.FloatField()
     purchase_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.code}: {self.name}"
