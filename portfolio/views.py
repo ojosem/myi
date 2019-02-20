0bae129caff093a9938405fafb02a7ec8351892a
Oliver Josem <omjosem@gmail.com>
2019-02-20 23:53:48 +1100

    Add Investment annotation to Portfolio and format numbers

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+from django.db.models import F, ExpressionWrapper, FloatField
 
 from .models import Stock
 
@@ -8,5 +9,11 @@ from .models import Stock
 
 
 def index(request):
-    stocks = Stock.objects.all()
+    # stocks = Stock.objects.all()
+    stocks = Stock.objects.annotate(
+        investment=ExpressionWrapper(
+            F("units") * F("purchase_price"), output_field=FloatField()
+        )
+    )
+
     return render(request, "portfolio/index.html", {"stocks": stocks})
