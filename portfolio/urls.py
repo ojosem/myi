9bc31e42869409ab3a6d013a78ea1128eb347680
Oliver Josem <omjosem@gmail.com>
2019-02-21 23:08:22 +1100

    Added Export CSV functionality to Portfolio

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'portfolio'
+app_name = "portfolio"
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path("", views.index, name="index"),
+    path("export/", views.export_csv, name="export"),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,11 +1,11 @@
+import csv
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 from django.db.models import F, ExpressionWrapper, FloatField
 
 from .models import Stock
-
-# Create your views here.
 
 
 def index(request):
@@ -17,3 +17,21 @@ def index(request):
     )
 
     return render(request, "portfolio/index.html", {"stocks": stocks})
+
+
+def export_csv(request):
+    field_names = [field.name for field in Stock._meta.fields][1:] + ["investment"]
+    stocks = Stock.objects.annotate(
+        investment=ExpressionWrapper(
+            F("units") * F("purchase_price"), output_field=FloatField()
+        )
+    )
+    print(field_names)
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = "attachment; filename=portfolio.csv"
+
+    writer = csv.writer(response)
+    writer.writerow(field_names)
+    for obj in stocks:
+        writer.writerow([getattr(obj, field) for field in field_names])
+    return response
